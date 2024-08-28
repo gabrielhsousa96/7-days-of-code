332918367c5c7e422425913d1064514d29e46a5f
gabrielhsousa96 <gabrielh.sousaf96@gmail.com>
2024-08-27 22:28:47 -0300

    Uptadate day 03
    Uptadate day 03

--- Day 03/app.js
+++ Day 03/app.js
@@ -11,50 +11,65 @@ function showText(tag, text){
     campo.innerHTML = text;
 };
 
-function startQuestions (){
-    let buttonStart = document.getElementById('c__Button');
-    let questionOne = document.getElementById('q1');
-    let bQuestionOne = document.getElementById('q1Buttons');
-    let bQuestionOneSend = document.getElementById('c__Button1');
-    buttonStart = changeClass('container__main__button', 'container__main__button__disabled');
-    questionOne = changeClass('container__question__1__disabled', 'container__question__1') ;
-    bQuestionOne = changeClass('container__question__1__buttons__disabled', 'container__question__1__buttons');
-    bQuestionOneSend = changeClass('container__button__1__disabled', 'container__button__1');
-    showText('p', 'Voce prente estudar BackEnd ou o FrontEND?');
+function startQuestions() {
+    changeClass('container__main__button', 'container__main__button__disabled');
+    activateFirstQuestion();
+    showText('p', 'Você pretende estudar Back-End ou Front-End?');
 }
 
-function secondQuestion(){
-    let endQ1 = document.getElementById('q1Buttons');
-    let endQ1Buttons = document.getElementById('c__Button1');
-    let buttonQ2 = document.getElementById('c__Button2');
-    let questionTwo = document.getElementById('q2');
-    let checkBox1 = document.getElementById('input__1');
-    let checkBox2 = document.getElementById('input__2');
-    let frontEnd = document.getElementById('frontend');
-    let backEnd = document.getElementById('backend');
-    let checkBox1Checked = checkBox1.checked;
-    let checkBox2Checked = checkBox2.checked;
-    endQ1 = changeClass('container__question__1__buttons', 'container__question__1__buttons__disabled');
-    endQ1Buttons = changeClass('container__button__1','container__button__1__disabled');
-    questionTwo = changeClass('container__question__2__disabled','container__question__2') ;
-    if (checkBox1Checked && checkBox2Checked){
-        frontEnd = changeClass('frontEnd__disabled', 'frontEnd');
-        backEnd = changeClass('backEnd__disabled', 'backEnd');
-        buttonQ2 = changeClass('container__button__2__disabled', 'container__button__2');
-        showText('p', 'Então voce planeja ser um desenvolvedor FullStack. Mas por quais linguagens voce pretende começar a estudar?')
+function activateFirstQuestion() {
+    changeClass('container__question__1__disabled', 'container__question__1');
+    changeClass('container__question__1__buttons__disabled', 'container__question__1__buttons');
+    changeClass('container__button__1__disabled', 'container__button__1');
+}
+
+
+function secondQuestion() {
+    const checkBox1Checked = document.getElementById('input__1').checked;
+    const checkBox2Checked = document.getElementById('input__2').checked;
+
+    changeClass('container__question__1__buttons', 'container__question__1__buttons__disabled');
+    changeClass('container__button__1', 'container__button__1__disabled');
+    changeClass('container__question__2__disabled', 'container__question__2');
+
+    if (checkBox1Checked && checkBox2Checked) {
+        activateFullStackPath();
+    } else if (checkBox1Checked) {
+        activateFrontEndPath();
+    } else if (checkBox2Checked) {
+        activateBackEndPath();
+    } else {
+        revertFirstQuestion();
     }
-    if( checkBox1Checked == true && checkBox2Checked == false ){
-        frontEnd = changeClass('frontEnd__disabled', 'frontEnd');
-        buttonQ2 = changeClass('container__button__2__disabled', 'container__button__2');
-        showText('p', 'Então voce planeja ser um desenvolvedor FrontEnd. Mas por quais linguagens voce pretende começar a estudar?')
-    } else if (checkBox1Checked == false && checkBox2Checked == true){
-        backEnd = changeClass('backEnd__disabled', 'backEnd');
-        buttonQ2 = changeClass('container__button__2__disabled', 'container__button__2');
-        showText('p', 'Então que voce planeja ser um desenvolvedor BackEnd. Mas por quais linguagens voce pretende começar a estudar?')
-    } else if (checkBox1Checked == false && checkBox2Checked == false){
-        endQ1 = changeClass('container__question__1__buttons__disabled','container__question__1__buttons');
-        endQ1Buttons = changeClass('container__button__1__disabled','container__button__1');
-        showText('p', 'Voce ainda não escolheu o que fazer? Comece escolhendo uma das etapas acima, antes de seguirmos.') }
+}
+
+function activateFullStackPath() {
+    changeClass('frontEnd__disabled', 'frontEnd');
+    changeClass('backEnd__disabled', 'backEnd');
+    activateButtonQ2();
+    showText('p', 'Então você planeja ser um desenvolvedor FullStack. Mas por quais linguagens você pretende começar a estudar?');
+}
+
+function activateFrontEndPath() {
+    changeClass('frontEnd__disabled', 'frontEnd');
+    activateButtonQ2();
+    showText('p', 'Então você planeja ser um desenvolvedor FrontEnd. Mas por quais linguagens você pretende começar a estudar?');
+}
+
+function activateBackEndPath() {
+    changeClass('backEnd__disabled', 'backEnd');
+    activateButtonQ2();
+    showText('p', 'Então que você planeja ser um desenvolvedor BackEnd. Mas por quais linguagens você pretende começar a estudar?');
+}
+
+function activateButtonQ2() {
+    changeClass('container__button__2__disabled', 'container__button__2');
+}
+
+function revertFirstQuestion() {
+    changeClass('container__question__1__buttons__disabled', 'container__question__1__buttons');
+    changeClass('container__button__1__disabled', 'container__button__1');
+    showText('p', 'Você ainda não escolheu o que fazer? Comece escolhendo uma das etapas acima, antes de seguirmos.');
 }
 
 function changeClass(oldClass, newClass){
